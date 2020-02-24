816d17b35bcd71dd6a2016b2e3bc594c3213bc8e
techLord-pm <37568342+techLord-pm@users.noreply.github.com>
2020-02-24 00:23:39 -0700

    Update main.py
    Websocket addition

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 import pandas as pd
 import numpy as np
+import websocket
+#https://pypi.org/project/websocket_client/
 
+def on_message(ws, message):
+    print(message)
 
-def main():
+def on_error(ws, error):
+    print(error)
+
+def on_close(ws):
+    print("### closed ###")
+
+def on_open(ws):
+    ws.send('{"type":"subscribe","symbol":"AAPL"}')
+    ws.send('{"type":"subscribe","symbol":"AMZN"}')
+    ws.send('{"type":"subscribe","symbol":"BINANCE:BTCUSDT"}')
+    ws.send('{"type":"subscribe","symbol":"IC MARKETS:1"}')
+
+if __name__ == "__main__":
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("wss://ws.finnhub.io?token=bp9nhovrh5rf91tippdg",
+                              on_message = on_message,
+                              on_error = on_error,
+                              on_close = on_close)
+    ws.on_open = on_open
+    ws.run_forever()
+
+def hello():
      print "hello world!"
      
